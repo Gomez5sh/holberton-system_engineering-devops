8f36591570ad0d0a256d9ee846fdb923561d8401
g5sh <eagomez2387@gmail.com>
2020-05-04 18:56:11 -0500

    [FIX] point 0: Fix point 0 * 1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,10 +7,10 @@ from sys import argv
 
 if __name__ == "__main__":
 
-    user_id = requests.get("https://jsonplaceholder.typicode.com/users?id="
-                           + argv[1])
-    todo_id = requests.get("https://jsonplaceholder.typicode.com/todos?userId="
-                           + argv[1])
+    user_id = requests.get(
+        "https://jsonplaceholder.typicode.com/users?id=" + argv[1])
+    todo_id = requests.get(
+        "https://jsonplaceholder.typicode.com/todos?userId=" + argv[1])
 
     json_user = user_id.json()
     json_todo = todo_id.json()
@@ -18,16 +18,16 @@ if __name__ == "__main__":
     total_task = 0
     titles = []
 
-    for pointer in json_todo:
-        if pointer['completed'] is True:
-            complete_task += 1
-            titles.append(pointer['title'])
-        total_task += 1
+    name = json_user[0]['name']
 
-    name_id = json_user[0]['name']
-
-    print("Employee {} s done with tasks({}/{}):"
-          .format(name_id, complete_task, total_task))
-
-    for task in titles:
-        print('\t' + task)
+    for task in json_todo:
+        if task['userId'] == int(argv[1]):
+            if task['completed'] is True:
+                complete_task += 1
+                titles.append(task['title'])
+            total_task += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, complete_task, total_task))
+    for title in titles:
+        print('\t ', end="")
+        print(title)
