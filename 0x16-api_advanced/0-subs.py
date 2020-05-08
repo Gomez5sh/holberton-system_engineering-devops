7f993bece5491d5f1891e509d9c21b69b58eb635
g5sh <eagomez2387@gmail.com>
2020-05-07 19:39:50 -0500

    [ADD] FEATURE: Add point 0 * fix * 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     user = {'User-agent': 'my-app/0.0.1'}
     fetch = requests.get('http://www.reddit.com/r/{}/about.json'
-                       .format(subreddit), headers=user)
+                         .format(subreddit), headers=user)
     try:
         fetch = fetch.json().get('data')
         for key, values in fetch.items():
